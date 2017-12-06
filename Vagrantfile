34db392a096941ddb9c77de7ac0b6e44ef227de7
Honpray <hanbing.leng@sjsu.edu>
2017-12-05 16:26:51 -0800

    Go updated.

--- Vagrantfile
+++ Vagrantfile
@@ -69,7 +69,6 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "shell_provisioner/npm_nodejs_mongo.sh"
   config.vm.provision "shell", path: "shell_provisioner/coffeescript.sh"
   config.vm.provision "shell", path: "shell_provisioner/gdb.sh"
-
-  # config.vm.provision "shell", path: "shell_provisioner/lua.sh"
-  # config.vm.provision "shell", path: "shell_provisioner/go.sh"
+  config.vm.provision "shell", path: "shell_provisioner/lua.sh"
+  config.vm.provision "shell", path: "shell_provisioner/go.sh"
 end
--- shell_provisioner/go.sh
+++ shell_provisioner/go.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get -y update
-wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz > /dev/null 2>&1
-tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz > /dev/null 2>&1
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=/vagrant_data/go
+#wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz > /dev/null 2>&1
+#tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz > /dev/null 2>&1
+#export PATH=$PATH:/usr/local/go/bin
+#export GOPATH=/vagrant_data/go
+apt-get install -y golang-go
